7e0edb4f96d721f0f795da089e2c0524d8761d99
NRossmann <NRossmann@gmx.de>
2023-12-10 21:20:00 +0000

    zsh änderungen

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,7 +2,14 @@
 alias nvim="~/.appImage/nvim.appimage"
 alias vim="nvim"
 
+#ls aliases
+alias ls="ls --color=auto"
+alias la="ls -la"
+alias ll="ls -l"
+
+
 #Misc
 alias zsh-reload="source ~/.zshrc"
 alias addalias="nvim ~/dotfiles/zsh/.config/zsh/aliases.zsh"
-
+alias zshconfig="nvim ~/dotfiles/zsh/.zshrc/"
+alias p10kconfig="nvim ~/dotfiles/zsh/.p10k.zsh"
--- zsh/.config/zsh/functions.zsh
+++
@@ -1,6 +0,0 @@
-#Shell Funktionen
-
-#NTFY Benachrichtigung
-function alertme(){
-  curl -d "$1" 139-162-144-34.ip.linodeusercontent.com/UjLbKJDfEc6qnvrG
-}
