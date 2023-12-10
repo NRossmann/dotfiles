8872cb0125018f679d9bc54dedf638de2244b84a
Nic Rossmann <NRossmann@gmx.de>
2023-12-10 19:30:32 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -11,7 +11,7 @@ mkdir .appImage;
 cd .appImage;
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage;
 chmod u+x nvim.appimage;
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
 
 #Erstelle config Ordner
 cd;
