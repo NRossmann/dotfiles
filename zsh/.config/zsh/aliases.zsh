303b9804a3a0632244fed2e3a3d72eb4ac1fd0be
NRossmann <NRossmann@gmx.de>
2025-09-16 12:36:06 +0200

    Removed oh my zsh

---
+++ zsh/.config/zsh/aliases.zsh
@@ -0,0 +1,21 @@
+###############################################################################
+# Aliases & Functions (Oh My Zsh custom)
+# File: ~/.oh-my-zsh/custom/aliases.zsh
+# Purpose: Aliases for ZSH
+###############################################################################
+
+# -----------------------------------------------------------------------------
+# Editor & dotfiles
+# -----------------------------------------------------------------------------
+alias zshconfig="nvim ~/.zshrc"
+alias zshreload="source ~/.zshrc"
+alias zshaliasconfig="nvim ~/.config/zsh/aliases.zsh"
+
+alias tmuxconfig="nvim ~/.config/tmux/tmux.conf"
+
+alias nvimconfig="nvim ~/.config/nvim/init.lua"
+
+alias sudoedit="sudo -e"
+###############################################################################
+# End of file
+###############################################################################
